a3d38cb1d3119219394a878e712b2f04cdd35b7e
ncou <nicolas_courade@yahoo.fr>
2016-12-26 14:21:36 +0100

    Update with the v4.0.0 pattern
    https://github.com/jquery-boilerplate/jquery-boilerplate/edit/master/dist/jquery.boilerplate.js

--- patterns/jquery.basic.plugin-boilerplate.js
+++ patterns/jquery.basic.plugin-boilerplate.js
@@ -1,76 +1,74 @@
-/*!
- * jQuery lightweight plugin boilerplate
- * Original author: @ajpiano
- * Further changes, comments: @addyosmani
- * Licensed under the MIT license
+/*
+ *  jquery-boilerplate - v4.0.0
+ *  A jump-start for jQuery plugins development.
+ *  http://jqueryboilerplate.com
+ *
+ *  Made by Zeno Rocha
+ *  Under MIT License
  */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;( function( $, window, document, undefined ) {
 
-// the semi-colon before the function invocation is a safety
-// net against concatenated scripts and/or other plugins
-// that are not closed properly.
-;(function ( $, window, document, undefined ) {
+	"use strict";
 
-    // undefined is used here as the undefined global
-    // variable in ECMAScript 3 and is mutable (i.e. it can
-    // be changed by someone else). undefined isn't really
-    // being passed in so we can ensure that its value is
-    // truly undefined. In ES5, undefined can no longer be
-    // modified.
+		// undefined is used here as the undefined global variable in ECMAScript 3 is
+		// mutable (ie. it can be changed by someone else). undefined isn't really being
+		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+		// can no longer be modified.
 
-    // window and document are passed through as local
-    // variables rather than as globals, because this (slightly)
-    // quickens the resolution process and can be more
-    // efficiently minified (especially when both are
-    // regularly referenced in your plugin).
+		// window and document are passed through as local variable rather than global
+		// as this (slightly) quickens the resolution process and can be more efficiently
+		// minified (especially when both are regularly referenced in your plugin).
 
-    // Create the defaults once
-    var pluginName = "defaultPluginName",
-        defaults = {
-            propertyName: "value"
-        };
+		// Create the defaults once
+		var pluginName = "defaultPluginName",
+			defaults = {
+				propertyName: "value"
+			};
 
-    // The actual plugin constructor
-    function Plugin( element, options ) {
-        this.element = element;
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+			this.element = element;
 
-        // jQuery has an extend method that merges the
-        // contents of two or more objects, storing the
-        // result in the first object. The first object
-        // is generally empty because we don't want to alter
-        // the default options for future instances of the plugin
-        this.options = $.extend( {}, defaults, options) ;
+			// jQuery has an extend method which merges the contents of two or
+			// more objects, storing the result in the first object. The first object
+			// is generally empty as we don't want to alter the default options for
+			// future instances of the plugin
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
 
-        this._defaults = defaults;
-        this._name = pluginName;
+		// Avoid Plugin.prototype conflicts
+		$.extend( Plugin.prototype, {
+			init: function() {
 
-        this.init();
-    }
+				// Place initialization logic here
+				// You already have access to the DOM element and
+				// the options via the instance, e.g. this.element
+				// and this.settings
+				// you can add more functions like the one below and
+				// call them like the example bellow
+				this.yourOtherFunction( "jQuery Boilerplate" );
+			},
+			yourOtherFunction: function( text ) {
 
-    Plugin.prototype = {
+				// some logic
+				$( this.element ).text( text );
+			}
+		} );
 
-        init: function() {
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.options
-            // you can add more functions like the one below and
-            // call them like so: this.yourOtherFunction(this.element, this.options).
-        },
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			} );
+		};
 
-        yourOtherFunction: function(el, options) {
-            // some logic
-        }
-    };
-
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName,
-                new Plugin( this, options ));
-            }
-        });
-    };
-
-})( jQuery, window, document );
+} )( jQuery, window, document );
